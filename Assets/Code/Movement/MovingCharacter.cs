5cfa64be07f31c6598dc48969c08331c809dd9d4
ray <rsmith52@wisc.edu>
2020-07-28 14:11:46 -0500

    Added game directory, currently setup with basic overworld movement

---
+++ Assets/Code/Movement/MovingCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Movement
+{
+    public abstract class MovingCharacter : MonoBehaviour
+    {
+        #region Constants
+
+        protected static readonly int WALK_PROPERTY = Animator.StringToHash("Walk");
+        protected static readonly float GROUND_MARGIN = 0.1f;
+
+        #endregion
+
+
+        #region Inspector
+
+        [SerializeField]
+        protected float speed = 1f;
+
+        [Header("Relations")]
+        [SerializeField]
+        protected Animator animator = null;
+
+        [SerializeField]
+        protected Rigidbody physicsBody = null;
+
+        [SerializeField]
+        protected CapsuleCollider collider = null;
+
+        [SerializeField]
+        protected SpriteRenderer spriteRenderer = null;
+
+        #endregion
+
+
+        #region Fields
+
+        protected Vector3 _movement;
+
+        #endregion
+
+    }
+}
